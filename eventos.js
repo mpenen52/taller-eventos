ea76dadd974e0f58460b093c8b8bcd1fe844101b
mpenen52 <mpenen52@gmail.com>
2023-09-03 22:49:26 -0300

    Solucion click-boton
    Al hacer clic sobre el botón ya no se despliega la alerta propia del div.

--- eventos.js
+++ eventos.js
@@ -1,5 +1,8 @@
 var div = document.getElementById("boton");
 
-div.addEventListener("click", function alertaDiv() {
+div.addEventListener("click", function(event) {
+    if (event.target !== this) {
+        return;
+    }
     alert("Hola! Soy el div");
 });
